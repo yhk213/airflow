4f0cf51d84e62ab1eb160e88fc44d87cda522970
yhk <younghae.kim.6405@gmail.com>
2024-01-15 12:39:01 +0900

    python import func 재수정

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -6,7 +6,7 @@ from common.common_func import get_sftp
 
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_import_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup=False
