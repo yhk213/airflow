f28cc099a536b533718e6e4c0a4a4ac11a6b861b
yhk <younghae.kim.6405@gmail.com>
2024-01-16 12:00:58 +0900

    task_decorator 업로드

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator 
+from airflow.decorators import task
+
+
+with DAG(
+    dag_id="dags_python_task_operator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    @task(task_id="python_task_1")
+    def print_context(some_input):
+        print(some_input)
+    
+    python_task_1   = print_context('task decorator 실행')
