6588c296a88da044e3dc8e171276805560204182
yhk <younghae.kim.6405@gmail.com>
2024-01-17 11:55:11 +0900

    kwargs practice

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator 
+from common.common_func import regist2
+
+
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag: 
+    
+    regist_t2 = PythonOperator(
+        task_id='regist_t2',
+        python_callable=regist2,
+        op_args=['yhk', 'male', 'Korea', 'Seoul']
+        op_kwargs={'email' : 'abc@gmail.net', 'phone': '010-000-0000'}
+    )
+
+    regist_t2
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -7,3 +7,15 @@ def regist(name, sex, *args):
     print(f'name : {name}')
     print(f'sex : {sex}')
     print(f'the others : {args}')
+
+
+def regist2(name, sex, *args, **kwargs):
+    print(f'name : {name}')
+    print(f'sex : {sex}')
+    print(f'the others : {args}')
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+    if email:
+        print(email)
+    if phone:
+        print(phone)
