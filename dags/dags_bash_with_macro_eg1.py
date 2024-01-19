6eb6adbdd84aa9c5b2f5ae2af94b9425534e92df
yhk <younghae.kim.6405@gmail.com>
2024-01-19 12:07:28 +0900

    macro datetime practice!

---
+++ dags/dags_bash_with_macro_eg1.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator 
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1",
+    schedule="10 0 L * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # start data : 전월 말일 , End_date : 1일전 
+    bash_task_1 = BashOperator(
+        task_id = 'bash_task_1',
+        env = {
+            'START_DATE' : '{{ data_interval_start.in_timezone("Asia/Seoul") | ds}}',
+            'END_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}'
+        },
+        bash_command = 'echo "START_DATE $START_DATE" && echo "END_DATE : $END_DATE"'
+    )
---
+++ dags/dags_bash_with_macro_eg2.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator 
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg2",
+    schedule="10 0 * * 6#2",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # start data : 2주전 월요일 , End_date : 2주전 토요일 
+    bash_task_1 = BashOperator(
+        task_id = 'bash_task_1',
+        env = {
+            'START_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds }}',
+            'END_DATE' : '{{(data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds }}'
+        },
+        bash_command = 'echo "START_DATE $START_DATE" && echo "END_DATE : $END_DATE"'
+    )
+
+    
