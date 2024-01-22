255af0acb6eda398e43b2184022a7986321a82cd
yhk <younghae.kim.6405@gmail.com>
2024-01-22 12:11:04 +0900

    pythonoperator with Macro 수정'

--- dags/dag_python_with_macro.py
+++ dags/dag_python_with_macro.py
@@ -12,8 +12,8 @@ with DAG(
 ) as dag:
     
     @task(task_id='task_using_macro',
-          templates_dict = {'start_date' : '{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) }}',
-                            'end_date' : '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) - macros.dateutil.relativedelta.relativedelta(days=19)) }}'
+          templates_dict = {'start_date' : '{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
+                            'end_date' : '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
           }
     )
     def get_datetime_macro(**kwargs):
@@ -31,7 +31,7 @@ with DAG(
 
         data_interval_end = kwargs['data_interval_end']
         prev_month_day_first = data_interval_end.in_timezone("Asia/Seoul") + relativedelta(months=-1, day=1)
-        prev_month_day_last = data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + relativedelta(days=1)
+        prev_month_day_last = data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + relativedelta(days=-1)
         print(prev_month_day_first.strftime('%Y-%m-%d'))
         print(prev_month_day_last.strftime('%Y-%m-%d'))
         
