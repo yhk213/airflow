e3d99036e346be73574229a4e2cab1e4dbe3ce40
yhk <younghae.kim.6405@gmail.com>
2024-01-31 12:24:27 +0900

    trigger rule example

---
+++ dags/dags_python_with_trigger_rule_eg1.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator 
+from airflow.operators.bash import BashOperator
+from airflow.decorators import task
+from airflow.exceptions import AirflowException
+
+with DAG (
+    dag_id = 'dags_python_with_trigger_rule_eg1',
+    start_date=pendulum.datetime(2024,1,30, tz='Asia/Seoul'),
+    schedule = None,
+    catchup= False
+) as dag:
+    bash_upstream_1 = BashOperator(
+        task_id = 'bash_upstream_1',
+        bash_command= 'echo upstream1!'
+    )
+
+    @task(task_id = 'python_upstream_1')
+    def python_upstream_1():
+        raise AirflowException('downstream_1 Exception!')
+    
+    @task(task_id = 'python_upstream_2')
+    def python_upstream_2():
+        print('정상처리!')
+
+    @task(task_id = 'python_downstream_1', trigeer_rule = 'all_done')
+    def python_downstream_1():
+        print('정상처리_!!!!')
+
+    [bash_upstream_1, python_upstream_1(), python_upstream_2()] >> python_downstream_1()
