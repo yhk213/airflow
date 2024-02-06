c9d7734be9c63796cc53f282d4a7d99e40998a3e
yhk <younghae.kim.6405@gmail.com>
2024-02-06 17:54:19 +0900

    trigger dag operator

---
+++ dags/dags_trigger_dag_run_operator.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BaseOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+import pendulum
+
+with DAG (
+    dag_id = 'dags_trigger_dag_run_operator',
+    start_date = pendulum.datetime(2024,2,4, tz='Asia/Seoul'),
+    schedule = '30 9 * * *',
+    catchup= False
+) as dag :
+    
+    start_task = BaseOperator(
+        task_id = 'start_task',
+        bash_command = 'echo "Start !!" '
+    )
+
+    trigger_dag_task = TriggerDagRunOperator(
+        task_id = 'trigger_dag_task',
+        trigger_dag_id= 'dags_python_operator',
+        trigger_run_id= None,
+        execution_date= '{{data_interval_start}}',
+        reset_dag_run= True,
+        wait_for_completion=False,
+        poke_interval=60,
+        allowed_states=['success'],
+        failed_states=None
+    )
