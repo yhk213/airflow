43d4ed3b22e7c8b52a25210e96d57e9da148f9ef
yhk <younghae.kim.6405@gmail.com>
2024-02-27 17:43:40 +0900

    Seoul Api 2nd 수정"

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -47,12 +47,12 @@ class SeoulApiToCsvOperator(BaseOperator):
             'charset' : 'utf-8',
             'Accept' : '*/*'
         }
-        request_url = f'{base_url}/{start_row}/{end_row}/'
 
         if self.base_dt is not None:
             request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
         else:
             request_url = f'{base_url}/{start_row}/{end_row}'
+            
         response = requests.get(request_url, headers)
         contents = json.loads(response.text)
 
