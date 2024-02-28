dfc9bdaa9941acd4c351424af1d4d2037bfd6579
yhk <younghae.kim.6405@gmail.com>
2024-02-28 13:22:28 +0900

    서울 공공데이터 수정

--- dags/dags_seoul_api_connection.py
+++ dags/dags_seoul_api_connection.py
@@ -13,15 +13,15 @@ with DAG (
     tb_corona19_count_status = SeoulApiToCsvOperator(
         task_id = 'tb_corona19_count_status',
         dataset_nm='TbCorona19CountStatus',
-        path = 'opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
+        path = '/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
         file_name= 'TbCorona19CountStatus.csv'
     )
 
     tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
         task_id = 'tv_corona19_vaccine_stat_new',
         dataset_nm='tvCorona19VaccinestatNew',
-        path = 'opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
+        path = '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
         file_name= 'tvCorona19VaccinestatNew.csv'
     )
-
+    
     tb_corona19_count_status >> tv_corona19_vaccine_stat_new
