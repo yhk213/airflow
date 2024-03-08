02805572943dd9ff19fb32d990cd15694bd5af4d
yhk <younghae.kim.6405@gmail.com>
2024-03-08 13:04:51 +0900

    postgre sirect insert dag 2nd

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -19,7 +19,7 @@ with DAG (
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
                 msg = 'insert 수행'
-                sql = 'insert into py_opr_drct_insrt_values (%s, %s, %s, %s);'
+                sql = 'insert into py_opr_drct_insrt values (%s, %s, %s, %s);'
                 cursor.execute(sql, (dag_id, task_id, run_id, msg))
                 conn.commit()
 
